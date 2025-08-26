b10d5798b7f9e62fe6849ae2549c50ce038e4889
hanifstrong24 <hanifstrong24@gmail.com>
2025-08-27 00:26:04 +0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -25,14 +25,6 @@ export default function Page() {
         <HeroBaru />
       </div>
 
-      {/* <Hero /> */}
-      <div className="relative">
-        {/* Konten di atas background */}
-        <div className="relative z-10">
-          <br />
-        </div>
-      </div>
-
       <OurApproach />
 
       {/* <ServiceSection className="!pb-0" {...serviceSectionData} /> */}
